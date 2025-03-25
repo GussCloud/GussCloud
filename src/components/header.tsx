1b35a7b25412c6e7bda6ca8465cdda8f890266d5
Gustavo Amaral <56303449+GussCloud@users.noreply.github.com>
2025-03-25 01:56:36 -0300

    Atualizar componente Header: adicionar cantos arredondados e sombra

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-white rounded-lg shadow-md p-4">
+      <h1 className="text-2xl font-bold">Header</h1>
+    </header>
+  );
+};
+
+export default Header;
